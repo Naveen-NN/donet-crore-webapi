6cec2866d4a81d600b488d93747acbc09bfdf3ee
Naveen-NN <learnfromnaveen@gmail.com>
2020-06-08 10:05:34 +0530

    Todo Api Sample

---
+++ TodoApiSample/ApiEntities/TodoItem.cs
@@ -0,0 +1,9 @@
+namespace TodoApiSample.ApiEntities
+{
+    public class TodoItem
+    {
+        public int? Id { get; set; }
+        public string Name { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ TodoApiSample/Controllers/TodoController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using TodoApiSample.ApiEntities;  
+
+namespace TodoApiSample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TodoController : ControllerBase
+    {
+        public TodoController()
+        {
+
+        }
+
+        [HttpGet]
+        public IEnumerable<TodoItem> List()
+        {
+            return null;
+        }
+
+        [HttpGet("{id}")]
+        public TodoItem Get(int id)
+        {
+            return null;
+        }
+
+        [HttpPost]
+        public IActionResult Create(TodoItem item)
+        {
+            //Code to insert values into DB 
+            item.Id = 1;
+            return CreatedAtAction(nameof(Create), new { id =item.Id}, item);
+        }
+
+
+        [HttpPost]
+        [Route("bulkcreate")]
+        public IActionResult BulkCreate(IEnumerable<TodoItem> items)
+        {
+            //Code to insert values into DB 
+            return CreatedAtAction(nameof(BulkCreate), items);
+        }
+
+
+        [HttpPut]
+        public IActionResult Update(TodoItem item)
+        {
+            if(item == null || !item.Id.HasValue) return BadRequest();
+            
+            return Ok(item);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+    }
+}
